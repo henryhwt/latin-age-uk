961965b4e6a2292b6bb8fcd1aa3a6c16d7215696
henryhwt <102864464+henryhwt@users.noreply.github.com>
2025-02-13 20:20:07 +0000

    ts no check

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,46 +1,44 @@
+// @ts-nocheck
 
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-import { notFound } from 'next/navigation';
-import { routing } from '@/i18n/routing';
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
 import { Navbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
-import { setRequestLocale } from 'next-intl/server';
+import {setRequestLocale} from 'next-intl/server';
 
-import type { Metadata } from 'next';
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Latin Ageing Association',
-  description:
-    "We're empowering individuals within the Bolivian and Latin community by providing a supportive and inclusive environment. We strive to enhance social health and well-being, foster cultural appreciation, and build lasting connections that unite us all in a shared sense of community.",
-};
-
-interface LocaleLayoutProps {
-  children: React.ReactNode;
-  params: {
-    locale: 'en' | 'es';
-  };
+  description: "We're empowering individuals within the Bolivian and Latin community by providing a supportive and inclusive environment. We strive to enhance social health and well-being, foster cultural appreciation, and build lasting connections that unite us all in a shared sense of community.",
 }
-
-export default async function LocaleLayout({ children, params: { locale } }: LocaleLayoutProps) {
-  // Ensure that the incoming `locale` is valid
+export default async function LocaleLayout(
+  { children, params: { locale } }
+  : 
+  { children: React.ReactNode; params: 
+    { locale: 'en' | 'es' } 
+  }
+) 
+  {
   if (!routing.locales.includes(locale)) {
     notFound();
   }
-
+ 
   setRequestLocale(locale);
-
-  // Fetch messages based on the locale; ensure you pass 'locale'
-  const messages = await getMessages(locale);
-
+  const messages = await getMessages();
+ 
   return (
     <html lang={locale}>
       <body>
-        <Navbar />
+
+      <Navbar/>
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
-        <Footer />
+
+      <Footer/>
       </body>
     </html>
   );
