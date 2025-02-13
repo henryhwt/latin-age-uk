9add1f9e964ec0d78c03b8f8ef1f88838eaaa913
henryhwt <102864464+henryhwt@users.noreply.github.com>
2025-02-13 20:05:34 +0000

    layout types

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,36 +1,39 @@
 
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
 import { Navbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
-import {setRequestLocale} from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Latin Ageing Association',
-  description: "We're empowering individuals within the Bolivian and Latin community by providing a supportive and inclusive environment. We strive to enhance social health and well-being, foster cultural appreciation, and build lasting connections that unite us all in a shared sense of community.",
-}
-export default async function LocaleLayout({ children, params: { locale } }: { children: React.ReactNode; params: { locale: 'en' | 'es' } }) {
+  description:
+    "We're empowering individuals within the Bolivian and Latin community by providing a supportive and inclusive environment. We strive to enhance social health and well-being, foster cultural appreciation, and build lasting connections that unite us all in a shared sense of community.",
+};
+
+export default async function LocaleLayout({ children, params: { locale } }) {
+  // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale)) {
     notFound();
   }
- 
+
   setRequestLocale(locale);
-  const messages = await getMessages();
- 
+
+  // Fetch messages based on the locale
+  const messages = await getMessages(locale);
+
   return (
     <html lang={locale}>
       <body>
-
-      <Navbar/>
+        <Navbar />
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
-
-      <Footer/>
+        <Footer />
       </body>
     </html>
   );
