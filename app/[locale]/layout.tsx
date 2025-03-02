6664d14922edbfb325888b9225767fca3b8cd880
henryhwt <102864464+henryhwt@users.noreply.github.com>
2025-03-02 19:29:03 +0000

    locale as en or es

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -22,7 +22,7 @@ export default async function LocaleLayout({
   params: Promise<{ locale: string } >;
   }) {
     const {locale} = await params;
-  if (!routing.locales.includes(locale as any)) {
+  if (!routing.locales.includes(locale as "en"|"es")) {
     notFound();
   }
  
