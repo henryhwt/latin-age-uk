1e9483334e8337691c91e3fab00f2d8fb9b3fca1
henryhwt <102864464+henryhwt@users.noreply.github.com>
2025-03-02 20:00:55 +0000

    not found page test

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -2,5 +2,5 @@ import {useTranslations} from 'next-intl';
  
 export default function NotFoundPage() {
   const t = useTranslations('Homepage');
-  return <h1>404 Not Found</h1>;
+  return <h1>404 Not Found: {t('callToAction')}</h1>;
 }
